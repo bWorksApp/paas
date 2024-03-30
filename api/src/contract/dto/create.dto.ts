21328f31872f00752d98e47c2a7811782f42259e
saigonbitmaster <bitmaster.sg@gmail.com>
2024-03-30 13:24:35 +0700

    clean up

--- api/src/contract/dto/create.dto.ts
+++
@@ -1,4 +0,0 @@
-// Contract/dto/create-Contract.dto.ts
-import { BaseContractDto } from './base.dto';
-
-export class CreateContractDto extends BaseContractDto {}
--- api/src/contract/dto/update.dto.ts
+++
@@ -1,7 +0,0 @@
-// todo/dto/update-todo.dto.ts
-import { BaseContractDto } from './base.dto';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateContractDto extends PartialType(BaseContractDto) {
-  completedAt: Date;
-}
